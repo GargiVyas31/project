cbff0c0e41939a308e9797d2f6d3eb8e750a572c
Gargi Vyas <gargi.vyas@gmail.com>
2018-09-20 23:20:08 +0530

    Library search feature

--- Library/views.py
+++ Library/views.py
@@ -7,26 +7,41 @@ def tech(request):
     books = Tech_Model.objects.all()
 
     if request.method=='POST':
+        if 'search_lib' in  request.POST:
 
-        print("value of c",c)
-        print("Book id",request.POST.get("dynid"))
-
-        t=Tech_Model.objects.filter(id=request.POST.get("dynid") ).values('copies')
-        copies= t[0]['copies'] #3
-        print("copies initially",copies)
-
-        if  c<3 :
-           if (copies>0):
-               copies=copies-1
-               print("Copies later",copies)
-               t=Tech_Model.objects.get( id=request.POST.get("dynid"))
-               t.copies=copies
-               t.save()
-
-           c=c+1
+            search= request.POST.get('search_lib')
+            print("searched ",search)
+            s=Tech_Model.objects.filter(book_name__icontains=search.casefold())
+            return render(request,'Library/library-tech.html',{'books':s})
 
 
-        return render(request, 'Library/library-tech.html',{'books':books})
+        elif "dynid" in request.POST:
+
+            print("value of c",c)
+            print("Book id",request.POST.get("dynid"))
+
+            t=Tech_Model.objects.filter(id=request.POST.get("dynid") ).values('copies')
+            copies= t[0]['copies'] #3
+            print("copies initially",copies)
+
+            if  c<3 :
+               if (copies>0):
+                   copies=copies-1
+                   print("Copies later",copies)
+                   t=Tech_Model.objects.get( id=request.POST.get("dynid"))
+                   t.copies=copies
+                   t.save()
+
+               c=c+1
+
+            return render(request, 'Library/library-tech.html',{'books':books})
+
+        else :
+            print("book not found")
+            return render(request,'Library/library-tech.html',{'books':books})
+
+
+
 
 
     print("GET request")
@@ -37,9 +52,43 @@ def tech(request):
 def bio(request):
 
     books = Bio_Model.objects.all()
+    c=0
     if request.method=='POST':
-        print("POST request")
-        return render(request, 'Library/library-bio.html')
+         print("POST request")
+         if 'search_lib' in  request.POST:
+
+            search= request.POST.get('search_lib')
+            print("searched ",search)
+            s=Bio_Model.objects.filter(book_name__icontains=search.casefold())
+            return render(request,'Library/library-bio.html',{'books':s})
+
+
+         elif "dynid" in request.POST:
+
+            print("value of c",c)
+            print("Book id",request.POST.get("dynid"))
+
+            t=Bio_Model.objects.filter(id=request.POST.get("dynid") ).values('copies')
+            copies= t[0]['copies'] #3
+            print("copies initially",copies)
+
+            if  c<3 :
+               if (copies>0):
+                   copies=copies-1
+                   print("Copies later",copies)
+                   t=Bio_Model.objects.get( id=request.POST.get("dynid"))
+                   t.copies=copies
+                   t.save()
+
+               c=c+1
+
+            return render(request, 'Library/library-bio.html',{'books':books})
+
+         else :
+            print("book not found")
+            return render(request,'Library/library-bio.html',{'books':books})
+
+
 
 
         #print("GET request")
@@ -49,40 +98,125 @@ def bio(request):
 def elec(request):
 
     books = Elec_Model.objects.all()
+    c=0
     if request.method=='POST':
-        print("POST request")
-        return render(request, 'Library/library-elec.html')
+         print("POST request")
+         if 'search_lib' in  request.POST:
+
+            search= request.POST.get('search_lib')
+            print("searched ",search)
+            s=Elec_Model.objects.filter(book_name__icontains=search.casefold())
+            return render(request,'Library/library-elec.html',{'books':s})
 
 
-        #print("GET request")
+         elif "dynid" in request.POST:
+
+            print("value of c",c)
+            print("Book id",request.POST.get("dynid"))
+
+            t=Elec_Model.objects.filter(id=request.POST.get("dynid") ).values('copies')
+            copies= t[0]['copies'] #3
+            print("copies initially",copies)
+
+            if  c<3 :
+               if (copies>0):
+                   copies=copies-1
+                   print("Copies later",copies)
+                   t=Elec_Model.objects.get( id=request.POST.get("dynid"))
+                   t.copies=copies
+                   t.save()
+
+               c=c+1
+
+            return render(request, 'Library/library-elec.html',{'books':books})
+
+         else :
+            print("book not found")
+            return render(request,'Library/library-elec.html',{'books':books})
+
 
     return render(request,'Library/library-elec.html',{'books':books})
 
 def mech(request):
 
     books = Mech_Model.objects.all()
+    c=0
     if request.method=='POST':
-        print("POST request")
-        return render(request, 'Library/library-mech.html')
+         print("POST request")
+         if 'search_lib' in  request.POST:
+
+            search= request.POST.get('search_lib')
+            print("searched ",search)
+            s=Mech_Model.objects.filter(book_name__icontains=search.casefold())
+            return render(request,'Library/library-bio.html',{'books':s})
 
 
-        #print("GET request")
+         elif "dynid" in request.POST:
+
+            print("value of c",c)
+            print("Book id",request.POST.get("dynid"))
+
+            t=Mech_Model.objects.filter(id=request.POST.get("dynid") ).values('copies')
+            copies= t[0]['copies'] #3
+            print("copies initially",copies)
+
+            if  c<3 :
+               if (copies>0):
+                   copies=copies-1
+                   print("Copies later",copies)
+                   t=Mech_Model.objects.get( id=request.POST.get("dynid"))
+                   t.copies=copies
+                   t.save()
+
+               c=c+1
+
+            return render(request, 'Library/library-mech.html',{'books':books})
+
+         else :
+            print("book not found")
+            return render(request,'Library/library-mech.html',{'books':books})
+
 
     return render(request,'Library/library-mech.html',{'books':books})
 
 def chem(request):
 
     books = Chem_Model.objects.all()
-    #issued = Chem_Model.objects.filter('issued')
-    #copies = Chem_Model.objects.filter('copies')
+    c=0
+    if request.method=='POST':
+         print("POST request")
+         if 'search_lib' in  request.POST:
+
+            search= request.POST.get('search_lib')
+            print("searched ",search)
+            s=Chem_Model.objects.filter(book_name__icontains=search.casefold())
+            return render(request,'Library/library-chem.html',{'books':s})
 
 
+         elif "dynid" in request.POST:
 
-    if request.method=='POST' :
-        print("POST request")
-        return render(request, 'Library/library-chem.html')
+            print("value of c",c)
+            print("Book id",request.POST.get("dynid"))
 
+            t=Chem_Model.objects.filter(id=request.POST.get("dynid") ).values('copies')
+            copies= t[0]['copies'] #3
+            print("copies initially",copies)
 
-        #print("GET request")
+            if  c<3 :
+               if (copies>0):
+                   copies=copies-1
+                   print("Copies later",copies)
+                   t=Chem_Model.objects.get( id=request.POST.get("dynid"))
+                   t.copies=copies
+                   t.save()
+
+               c=c+1
+
+            return render(request, 'Library/library-chem.html',{'books':books})
+
+         else :
+            print("book not found")
+            return render(request,'Library/library-chem.html',{'books':books})
+
 
     return render(request,'Library/library-chem.html',{'books':books})
