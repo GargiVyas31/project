00b8ab81cdc47fa06a3b0fb2e3c5bb025ebe00f6
Gargi Vyas <gargi.vyas@gmail.com>
2018-09-25 04:59:19 +0530

    Removed Firebase

--- Teachers/views.py
+++ Teachers/views.py
@@ -1,21 +1,9 @@
 from django.shortcuts import render
 import pyrebase
-from firebase_admin import credentials
-config={
-    'apiKey': "AIzaSyAjR7Zu_ruf7twHyMh-OXr183t-TceVvTY",
-    'authDomain': "webst-c384e.firebaseapp.com",
-    'databaseURL': "https://webst-c384e.firebaseio.com",
-    'projectId': "webst-c384e",
-    'storageBucket': "webst-c384e.appspot.com",
-    'messagingSenderId': "251406846010",
-    #'serviceAccount': "C:/Users/maitr/Downloads/webst-c384e-firebase-adminsdk-d2hqm-fcb62dae59.json",
 
-    }
 
-firebase = pyrebase.initialize_app(config)
-auth = firebase.auth()
 
-database=firebase.database()
+
 # Create your views here.
 def mainprofile(request):
 	
